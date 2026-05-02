24fabfe40d392d8dceb4d6b46c62d2a479de8266
Luis Alejandro Esquivel Rojas <luisalejandroesquivelrojas@gmail.com>
2026-05-02 16:32:14 -0500

    fix

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,6 +1,8 @@
 import pymysql
 pymysql.install_as_MySQLdb()
 
+# No usamos Flask-MySQLdb, solo la compatibilidad
+# El objeto mysql se usa en el código como extensión
 from flask_mysqldb import MySQL
 
 mysql = MySQL()
